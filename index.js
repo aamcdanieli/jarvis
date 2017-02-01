5a1118f5a673f2676077d80dd4f1695bb0ec7c6d
Aaron McDaniel <aamcdaniel@tycoint.com>
2017-02-01 15:06:10 -0800

    Remove comma

--- index.js
+++ index.js
@@ -5,34 +5,8 @@ const fs = require('fs')
 const https = require('https')
 const app = express()
 
-https.createServer({
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-}, app).listen(443)
-
-function requestVerifier(req, res, next) {
-    alexaVerifier(
-        req.headers.signaturecertchainurl,
-        req.headers.signature,
-        req.rawBody,
-        function verificationCallback(err) {
-            if (err) {
-                res.status(401).json({ message: 'Verification Failure', error: err })
-            } else {
-                next()
-            }
-        }
-    )
-}
-
-app.post('/recipeIntent', requestVerifier, function(req, res) {
-    // We'll fill this out later!
-    res.json({ hello: 'world' });
-});
-
 app.get('/', (req, res) => {
   res.send('HEY!')
 })
-
 
 app.listen(3000, () => console.log('Server running on port 3000'))
